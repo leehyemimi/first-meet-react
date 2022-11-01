838379317f968666cc212fa46c3f1ab229333d6e
테스트 <test@gmail.com>
2022-11-01 15:56:32 +0900

    Revert "add"
    This reverts commit 559abe0d3da4f70d97ba68dd16dd85a4908428e9.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from './chapter_03/Library';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<Library />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
